552348a8b587f85fb666236eb4a03715b8b6ed41
Yasitha Bhanuka <yasithabhanukac@gmail.com>
2024-12-11 12:17:35 +0530

    Make the Admin controller, views autogenerated using MVC and make the configurations in the Program.cs file for add the Db context as a service. builder.Services.AddDbContext<OnlineshopContext>();

--- onlineshop/Program.cs
+++ onlineshop/Program.cs
@@ -1,8 +1,10 @@
+using onlineshop.Models.Db;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<OnlineshopContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

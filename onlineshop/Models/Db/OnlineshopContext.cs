6a28ab53f9f70641666f357987fc70bcb120e2e8
Yasitha Bhanuka <yasithabhanukac@gmail.com>
2024-12-11 11:46:12 +0530

    Add project files.

---
+++ onlineshop/Models/Db/OnlineshopContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace onlineshop.Models.Db;
+
+public partial class OnlineshopContext : DbContext
+{
+    public OnlineshopContext()
+    {
+    }
+
+    public OnlineshopContext(DbContextOptions<OnlineshopContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Menu> Menus { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-EC3L3D0;Database=onlineshop;Trusted_Connection=True;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Menu>(entity =>
+        {
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Link).HasMaxLength(300);
+            entity.Property(e => e.MenuTitle).HasMaxLength(100);
+            entity.Property(e => e.Type).HasMaxLength(30);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}

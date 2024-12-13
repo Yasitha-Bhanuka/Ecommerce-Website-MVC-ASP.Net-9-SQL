7d778267c287728e88c241c4ec26532a7a376fc6
Yasitha Bhanuka <yasithabhanukac@gmail.com>
2024-12-13 14:59:58 +0530

    make the delete image files from the application wwwroot/images/banners directory using BannersController.cs and Delete.cshtml files.

--- onlineshop/Areas/Admin/Controllers/BannersController.cs
+++ onlineshop/Areas/Admin/Controllers/BannersController.cs
@@ -102,7 +102,7 @@ namespace onlineshop.Areas.Admin.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,SubTitle,ImageName,Priority,Link,Position")] Banner banner)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,SubTitle,ImageName,Priority,Link,Position")] Banner banner, IFormFile? ImageFile)
         {
             if (id != banner.Id)
             {
@@ -113,6 +113,30 @@ namespace onlineshop.Areas.Admin.Controllers
             {
                 try
                 {
+                    //====================save image=====================
+                    if (ImageFile != null)
+                    {
+                        //-----------------
+                        string org_fn;
+                        org_fn = Directory.GetCurrentDirectory() + "/wwwroot/images/banners/" + banner.ImageName;
+
+                        if (System.IO.File.Exists(org_fn))
+                        {
+                            System.IO.File.Delete(org_fn);
+                        }
+                        //-----------------
+                        banner.ImageName = Guid.NewGuid() + Path.GetExtension(ImageFile.FileName);
+                        //-----------------
+                        string ImagePath;
+                        ImagePath = Directory.GetCurrentDirectory() + "\\wwwroot\\images\\banners\\" + banner.ImageName;
+
+                        using (var stream = new FileStream(ImagePath, FileMode.Create))
+                        {
+                            ImageFile.CopyTo(stream);
+                        }
+
+                    }
+                    //===================================================
                     _context.Update(banner);
                     await _context.SaveChangesAsync();
                 }
@@ -158,6 +182,17 @@ namespace onlineshop.Areas.Admin.Controllers
             var banner = await _context.Banners.FindAsync(id);
             if (banner != null)
             {
+                //===========================delete id====================
+                //-----------------
+                string org_fn;
+                org_fn = Directory.GetCurrentDirectory() + "/wwwroot/images/banners/" + banner.ImageName;
+
+                if (System.IO.File.Exists(org_fn))
+                {
+                    System.IO.File.Delete(org_fn);
+                }
+                //-----------------
+                //========================================================
                 _context.Banners.Remove(banner);
             }
 
